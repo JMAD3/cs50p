db90351f458eff827a3673cc4c572f8e42d0d395
James Mader <jamesmader10@gmail.com>
2026-07-21 07:39:21 -0500

    week4

---
+++ week4/libraries.py
@@ -0,0 +1,13 @@
+# module == library in python
+
+import random
+
+# or
+
+from random import choice
+
+print(random.choice(["heads","tails"]))
+
+# list slicing big.pizza[1:]
+
+#not a super complicated subject
